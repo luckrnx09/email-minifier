e046b61a1dbb9ab0da5417785ee07de361cd8319
EagleSoap <46696354+EagleSoap@users.noreply.github.com>
2023-12-22 22:01:41 +0800

    Feat: Add playground (#8)
    * Complete the mail compression function.
    
    * Modify the CSS.
    
    ---------
    
    Co-authored-by: EagleSoap <‘13540208438@163.com’>

---
+++ playground/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import areaIndex from "./view/index.vue";
+</script>
+
+<template>
+    <section class="view-index">
+        <area-index />
+    </section>
+</template>
+
+<style scoped>
+.view-index {
+    width: 100%;
+    height: 100vh;
+}
+</style>
+
